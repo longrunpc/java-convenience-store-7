d6f814ce68148767cbbd0ba0dd93d0d9479b3fa8
longrunpc <pcjmo4050@gmail.com>
2024-11-11 05:18:46 +0900

    feat(Receipt): Receipt 클래스에 할인 및 최종 금액 계산 기능 추가
    - excludedPromotionPrice 계산을 위한 addExcludedPromotionPrice 메서드 추가
    - 멤버십 할인 계산을 위한 calculateMembershipDiscount 메서드 추가
      - excludedPromotionPrice의 30%를 계산하며 최대 할인 한도는 8,000원으로 제한
    - 최종 결제 금액을 계산하는 calculateFinalPrice 메서드 추가
    - 멤버십 할인과 최종 금액을 반환하는 Getter 메서드 추가

--- src/main/java/store/model/Receipt.java
+++ src/main/java/store/model/Receipt.java
@@ -8,9 +8,10 @@ public class Receipt {
     private final Map<String, ReceiptDetail> giveaway;
     public int totalQuantity;
     private int totalPrice;
+    private int excludedPromotionPrice;
     public int eventDiscount;
     private int membershipDiscount;
-    private int finalAmount;
+    private int finalPrice;
 
     public Receipt() {
         this.purchasedProducts = new HashMap<>();
@@ -19,18 +20,33 @@ public class Receipt {
         this.totalQuantity = 0;
         this.eventDiscount = 0;
         this.membershipDiscount = 0;
-        this.finalAmount = 0;
+        this.finalPrice = 0;
     }
 
     public void addPurchasedProduct(ReceiptDetail receiptDetail) {
         purchasedProducts.put(receiptDetail.getName(), receiptDetail);
-        totalPrice += (receiptDetail.getQuantity()*receiptDetail.getPrice());
+        totalPrice += (receiptDetail.getQuantity() * receiptDetail.getPrice());
         totalQuantity += receiptDetail.getQuantity();
+    }
+
+    public void addExcludedPromotionPrice(ReceiptDetail receiptDetail) {
+        excludedPromotionPrice += (receiptDetail.getQuantity()*receiptDetail.getPrice());
     }
 
     public void addGiveaway(ReceiptDetail receiptDetail) {
         giveaway.put(receiptDetail.getName(), receiptDetail);
-        eventDiscount += (receiptDetail.getQuantity()*receiptDetail.getPrice());
+        eventDiscount += (receiptDetail.getQuantity() * receiptDetail.getPrice());
+    }
+
+    public void calculateMembershipDiscount() {
+        membershipDiscount = (int) (excludedPromotionPrice * 0.3); // 30% 계산
+        if (membershipDiscount > 8000) {
+            membershipDiscount = 8000;
+        }
+    }
+
+    public void calculateFinalPrice() {
+        finalPrice = totalPrice - eventDiscount - membershipDiscount;
     }
 
     public int getTotalQuantity(){
@@ -47,5 +63,13 @@ public class Receipt {
 
     public ReceiptDetail getGiveaway(String productName) {
         return giveaway.get(productName);
+    }
+
+    public int getMembershipDiscount() {
+        return membershipDiscount;
+    }
+
+    public int getFinalPrice() {
+        return finalPrice;
     }
 }
--- src/test/java/store/model/ReceiptTest.java
+++ src/test/java/store/model/ReceiptTest.java
@@ -41,6 +41,40 @@ public class ReceiptTest {
 
         //then
         assertThat(receipt.getEventDiscount()).isEqualTo(1000);
+    }
+
+    @DisplayName("맴버십 할인 테스트")
+    @Test
+    void membershipDiscountTest() {
+        //given
+        ReceiptDetail receiptDetail = new ReceiptDetail("에너지바", 5, 2000);
+        receipt.addExcludedPromotionPrice(receiptDetail);
+
+        //when
+        receipt.calculateMembershipDiscount();
+
+        //then
+        assertThat(receipt.getMembershipDiscount()).isEqualTo(3000);
+    }
+
+    @DisplayName("최종 구매 금액 반환 테스트")
+    @Test
+    void finalPriceTest(){
+        //given
+        ReceiptDetail receiptDetail1 = new ReceiptDetail("콜라", 3, 1000);
+        ReceiptDetail receiptDetail2 = new ReceiptDetail("에너지바", 5, 2000);
+        ReceiptDetail testGiveaway = new ReceiptDetail("콜라", 1, 1000);
+        receipt.addPurchasedProduct(receiptDetail1);
+        receipt.addPurchasedProduct(receiptDetail2);
+        receipt.addGiveaway(testGiveaway);
+        receipt.addExcludedPromotionPrice(receiptDetail2);
+        receipt.calculateMembershipDiscount();
+
+        //when
+        receipt.calculateFinalPrice();
+
+        //then
+        assertThat(receipt.getFinalPrice()).isEqualTo(9000);
 
     }
 }
